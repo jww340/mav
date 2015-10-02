16c2daf0850e04d6d608d8b8e5f79e8d492d8fc4
Bryan A. Jones <bjones AT ece.msstate.edu>
2015-10-02 15:21:50 -0500

    Rename: myDialog to mavDialog. Fix: test_4 needs enter to be pressed.

--- homework/mav_gui_test.py
+++ homework/mav_gui_test.py
@@ -33,7 +33,7 @@ from mav_gui import MavDialog
 #    object.
 # 3. Use the function scope to run setup/teardown around every test.
 @pytest.fixture(scope='function')
-def myDialog(qtbot, request):
+def mavDialog(qtbot, request):
     # Setup.
     md = MavDialog(4)
     md.show()
@@ -48,35 +48,36 @@ def myDialog(qtbot, request):
 # ----------
 class TestMavGui(object):
     # Check that fly time slider changes update the text box.
-    def test_1(self, myDialog):
-        myDialog.hsFlyTime.setValue(10)
-        assert myDialog.leFlyTime.text() == '10'
+    def test_1(self, mavDialog):
+        mavDialog.hsFlyTime.setValue(10)
+        assert mavDialog.leFlyTime.text() == '10'
 
     # Check that fly time text box changes update the slider.
-    def test_2(self, myDialog, qtbot):
-        myDialog.leFlyTime.setText('20')
-        # Bug: On Windows, qtbot.keyClicks(myDialog.leFlyTime, '\n') crashes.
-        qtbot.keyClick(myDialog.leFlyTime, Qt.Key_Enter)
-        assert myDialog.hsFlyTime.value() == 20
+    def test_2(self, mavDialog, qtbot):
+        mavDialog.leFlyTime.setText('20')
+        # Bug: On Windows, qtbot.keyClicks(mavDialog.leFlyTime, '\n') crashes.
+        qtbot.keyClick(mavDialog.leFlyTime, Qt.Key_Enter)
+        assert mavDialog.hsFlyTime.value() == 20
 
     # Check that charge time slider changes update the text box.
-    def test_3(self, myDialog):
-        myDialog.hsChargeTime.setValue(10)
-        assert myDialog.leChargeTime.text() == '10'
+    def test_3(self, mavDialog):
+        mavDialog.hsChargeTime.setValue(10)
+        assert mavDialog.leChargeTime.text() == '10'
 
     # Check that charge time text box changes update the slider.
-    def test_4(self, myDialog):
-        myDialog.leChargeTime.setText('20')
-        assert myDialog.hsChargeTime.value() == 20
+    def test_4(self, mavDialog, qtbot):
+        mavDialog.leChargeTime.setText('20')
+        qtbot.keyClick(mavDialog.leChargeTime, Qt.Key_Enter)
+        assert mavDialog.hsChargeTime.value() == 20
 
     # Check that invalid strings aren't allowed in the edit boxes.
-    def test_5(self, myDialog, qtbot):
-        myDialog.leFlyTime.setText('30')
-        qtbot.keyClicks(myDialog.leFlyTime, 'hello')
-        assert myDialog.leFlyTime.text() == '30'
+    def test_5(self, mavDialog, qtbot):
+        mavDialog.leFlyTime.setText('30')
+        qtbot.keyClicks(mavDialog.leFlyTime, 'hello')
+        assert mavDialog.leFlyTime.text() == '30'
 
         # Must clear the text before trying to type more text.
-        myDialog.leFlyTime.setText('')
-        qtbot.keyClicks(myDialog.leFlyTime, '999')
-        assert myDialog.leFlyTime.text() == '99'
+        mavDialog.leFlyTime.setText('')
+        qtbot.keyClicks(mavDialog.leFlyTime, '999')
+        assert mavDialog.leFlyTime.text() == '99'
 
