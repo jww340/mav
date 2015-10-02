9a4e0f0caeff28c83161cded64412718f8b0da2e
Bryan A. Jones <bjones AT ece.msstate.edu>
2015-10-02 16:43:39 -0500

    Fix: Change time on sliders to be in tenths of a second. Add: Signal emission to MAV on slider update.

--- homework/mav_gui_test.py
+++ homework/mav_gui_test.py
@@ -23,7 +23,6 @@ from PyQt4.QtCore import Qt
 # -----
 from mav_gui import MavDialog
 #
-#
 # Testing
 # =======
 # Use a `fixture <https://pytest.org/latest/fixture.html>`_ to:
@@ -43,52 +42,59 @@ def mavDialog(qtbot, request):
     request.addfinalizer(md.terminate)
 
     return md
-
+#
 # TestMavGui
 # ----------
 class TestMavGui(object):
     # Check that fly time slider changes update the text box.
     def test_1(self, mavDialog):
-        mavDialog.hsFlyTime.setValue(10)
-        assert mavDialog.leFlyTime.text() == '10'
+        mavDialog.hsFlyTime.setValue(15)
+        assert float(mavDialog.leFlyTime.text()) == 1.5
 
     # Check that fly time text box changes update the slider.
     def test_2(self, mavDialog, qtbot):
-        mavDialog.leFlyTime.setText('20')
+        mavDialog.leFlyTime.setText('2.5')
         # Bug: On Windows, qtbot.keyClicks(mavDialog.leFlyTime, '\n') crashes.
         qtbot.keyClick(mavDialog.leFlyTime, Qt.Key_Enter)
-        assert mavDialog.hsFlyTime.value() == 20
+        assert mavDialog.hsFlyTime.value() == 25
 
     # Check that charge time slider changes update the text box.
     def test_3(self, mavDialog):
-        mavDialog.hsChargeTime.setValue(10)
-        assert mavDialog.leChargeTime.text() == '10'
+        mavDialog.hsChargeTime.setValue(35)
+        assert float(mavDialog.leChargeTime.text()) == 3.5
 
     # Check that charge time text box changes update the slider.
     def test_4(self, mavDialog, qtbot):
-        mavDialog.leChargeTime.setText('20')
+        mavDialog.leChargeTime.setText('4.5')
         qtbot.keyClick(mavDialog.leChargeTime, Qt.Key_Enter)
-        assert mavDialog.hsChargeTime.value() == 20
+        assert mavDialog.hsChargeTime.value() == 45
 
     # Check that invalid strings aren't allowed in the fly time edit boxe.
     def test_5(self, mavDialog, qtbot):
-        mavDialog.leFlyTime.setText('30')
+        mavDialog.leFlyTime.setText('5.5')
         qtbot.keyClicks(mavDialog.leFlyTime, 'hello')
-        assert mavDialog.leFlyTime.text() == '30'
+        assert mavDialog.leFlyTime.text() == '5.5'
 
         # Must clear the text before trying to type more text.
         mavDialog.leFlyTime.setText('')
         qtbot.keyClicks(mavDialog.leFlyTime, '999')
-        assert mavDialog.leFlyTime.text() == '99'
+        assert float(mavDialog.leFlyTime.text()) == 9.0
 
     # Check that invalid strings aren't allowed in the edit boxes.
     def test_6(self, mavDialog, qtbot):
-        mavDialog.leChargeTime.setText('30')
+        mavDialog.leChargeTime.setText('6.5')
         qtbot.keyClicks(mavDialog.leChargeTime, 'hello')
-        assert mavDialog.leChargeTime.text() == '30'
+        assert mavDialog.leChargeTime.text() == '6.5'
 
         # Must clear the text before trying to type more text.
         mavDialog.leChargeTime.setText('')
         qtbot.keyClicks(mavDialog.leChargeTime, '999')
-        assert mavDialog.leChargeTime.text() == '99'
-
+        assert float(mavDialog.leChargeTime.text()) == 9.0
+#
+# Test?
+# -----
+class TestStuff(object):
+    # Check that the GUI emits signals to the MAVs.
+    def test_1(self, mavDialog, qtbot):
+        with qtbot.waitSignal(mavDialog._chargingStation._mav[0].updateFlyTimeSec, raising=True):
+            mavDialog.hsChargeTime.setValue(75)
